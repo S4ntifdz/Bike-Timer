67ab1dc5f2e56db920e1b289a124e4c90f780a6f
S4ntifdz <safernandez@frro.utn.edu.ar>
2025-06-28 18:49:17 -0300

    WIP

--- public/sw.js
+++ public/sw.js
@@ -7,21 +7,9 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
 });
